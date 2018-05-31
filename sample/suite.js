8be6731fb881bb672a15a49cebce4d61369090e2
Ralf S. Engelschall <rse@engelschall.com>
2018-05-31 16:52:30 +0200

    improve suite

--- sample/suite.js
+++ sample/suite.js
@@ -4,28 +4,35 @@
 const expect = require("chai").expect
 const Mutex = require("..")
 
-module.exports = async function (url, id = 0) {
-    /*  open connection  */
-    console.log(`++ START ${id}: ${url}`)
-    let mutex = new Mutex(url)
-    await mutex.open()
+module.exports = (url, id = 0) => {
+    let delay = Math.trunc(Math.random() * 1 * 10)
+    setTimeout(async () => {
+        /*  open connection  */
+        console.log(`++ START ${id}: ${url}`)
+        let mutex = new Mutex(url)
+        await mutex.open()
 
-    for (let i = 0; i < 10; i++) {
-        await mutex.acquire()
-        console.log(`++ ACQUIRED ${id} (#${i})`)
-        await new Promise((resolve, reject) => {
-            let delay = Math.trunc(Math.random() * 1 * 100)
-            console.log(`++ WORK ${id} (#${i}): ${delay}ms`)
-            setTimeout(() => {
-                resolve()
-            }, delay)
-        })
-        console.log(`++ RELEASE ${id} (#${i})`)
-        await mutex.release()
-    }
+        for (let i = 0; i < 10; i++) {
+            await mutex.acquire()
+            // console.log(`++ ACQUIRED ${id} (#${i})`)
+            await new Promise((resolve, reject) => {
+                let delay = Math.trunc(Math.random() * 1 * 0)
+                console.log(`++ WORK ${id} (#${i}): ${delay}ms`)
+                setTimeout(() => {
+                    resolve()
+                }, delay)
+            })
+            // console.log(`++ RELEASE ${id} (#${i})`)
+            await mutex.release()
+            await new Promise((resolve) => {
+                let delay = Math.trunc(Math.random() * 1 * 500)
+                setTimeout(resolve, delay)
+            })
+        }
 
-    /*  close connection  */
-    await mutex.close()
-    console.log(`++ END ${id}`)
+        /*  close connection  */
+        await mutex.close()
+        console.log(`++ END ${id}`)
+    }, delay)
 }
 
