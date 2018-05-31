1e2dd83e8c91d3c942c6d9d4213753ed30d4a6d9
Ralf S. Engelschall <rse@engelschall.com>
2018-05-31 16:53:56 +0200

    cleanup

--- sample/rpm.js
+++ sample/rpm.js
@@ -3,7 +3,8 @@ const suite = require("./suite")
 
 ;(async () => {
     for (let i = 0; i < 10; i++)
-        suite("rpm+redis://x:local-secret@127.0.0.1:6379/test", i)
+        //suite("rpm+redis://x:local-secret@127.0.0.1:6379/test", i)
+        suite("rpm+consul://127.0.0.1:8500/test", i)
 })().catch((err) => {
     console.log(`ERROR: ${err}`)
 })
