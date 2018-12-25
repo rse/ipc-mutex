f43affd1c11bb219f2ec2ce9d2b4d964453bf122
Ralf S. Engelschall <rse@engelschall.com>
2018-12-25 16:27:06 +0100

    add EtcD support

---
+++ sample/rpm-etcd.js
@@ -0,0 +1,34 @@
+
+/* eslint no-console: off */
+
+const Suite   = require("./suite")
+const cluster = require("cluster")
+
+;(async () => {
+    let workers = 10
+    if (cluster.isMaster) {
+        for (let i = 0; i < workers; i++)
+            setTimeout(() => cluster.fork(), 0)
+        cluster.on("exit", (worker, code, signal) => {
+            workers--
+        })
+    }
+    let suite = new Suite("rpm+etcd://127.0.0.1:2379/test",
+        cluster.isMaster ? "MASTER" : `WORKER-${process.pid}`)
+    await suite.open()
+    await suite.work()
+    if (cluster.isMaster) {
+        let timer = setInterval(async () => {
+            if (workers === 0) {
+                clearTimeout(timer)
+                await suite.close()
+                process.exit(0)
+            }
+        }, 100)
+    }
+    else {
+        await suite.close()
+        process.exit(0)
+    }
+})()
+
